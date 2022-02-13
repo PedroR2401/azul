8f5f587e3ff2695b8735c3bfcbb82fa14398eec1
PedroR2401 <99568069+PedroR2401@users.noreply.github.com>
2022-02-12 22:38:29 -0300

    commit inicial

---
+++ parker.js
@@ -0,0 +1,15 @@
+
+window.alert("Olá mundo!")
+var n = window.prompt("em que ano você nasceu?")
+var d = 2022 - n 
+
+window.alert(`Você tem ` + d + ` anos`);
+if ( d < 18)
+ {window.alert ("Você é menor de idade, VAZA!!!!!")
+ window.location.href = "https://store.steampowered.com/?l=portuguese";}
+
+else if (d >= 18) {
+ window.alert("Você pode ficar!")}
+ 
+
+   
